3933c4ca64d7590c90bf3db27e669718b89c7cb7
Jay Zawrotny <jayzawrotny@gmail.com>
2020-03-03 23:30:16 -0500

    Cleaned up timing mechanism of mock mongo stream

--- mock-mongo.js
+++ mock-mongo.js
@@ -1,4 +1,29 @@
+const _ = require("highland");
 const { Readable } = require("stream");
+
+// delay :: Number
+//          -> (Error | NULL, *, (Error | NULL, *) -> *, () -> *)
+//          -> undefined
+// A highland consume function to delay each value by a specified number of ms.
+// Takes a number of MS to delay
+// Returns undefined
+function delay (ms) {
+  return (err, x, push, next) => {
+    if (err) {
+      push(err);
+      next();
+    }
+    else if (x === _.nil) {
+      push(null, x);
+    }
+    else {
+      setTimeout(() => {
+        push(null, x);
+        next();
+      }, ms);
+    }
+  };
+}
 
 // MockMongo :: MockMongo({
 //   docs: [{ _id: Number }],
@@ -9,34 +34,30 @@ module.exports = class MockMongo extends Readable {
     super(Object.assign({}, options, {
       objectMode: true,
       autoDestroy: true,
-      _docs: options.docs,
     }));
 
     this._docs = options.docs;
     this._interval = options.interval;
-    this._timer = null;
+    this._source = null;
   }
 
-  // timeout :: Number -> *
-  // Loops a timeout to push docs at a given delay interval.
-  // Takes a seed number and emits docs at given index.
-  // Returns nothing
-  loopTimeout (docs) {
-    if (docs.length === 0) {
-      this.push(null);
-      return;
-    }
-    this._timer = setTimeout(() => {
-      this.push(docs[0]);
-      this.loopTimeout(docs.slice(1));
-    }, this._interval);
+  startReading () {
+    this._source = _(this._docs);
+
+    this._source
+      .consume(delay(this._interval))
+      .each(doc => this.push(doc))
+      .done(() => this.stopReading());
+  }
+
+  stopReading () {
+    this.push(null);
+    this._source = null;
   }
 
   _read (size) {
-    if (!this._timer) {
-      setTimeout(() => {
-        this.loopTimeout(this._docs);
-      }, 3000);
+    if (!this._source) {
+      this.startReading();
     }
   }
 }
